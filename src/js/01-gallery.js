7d3e19ce1b25ebb6755088b85bac2b334f5db23a
Iuliia Sokolovska <uu.sokil@gmail.com>
2022-10-21 23:00:25 +0200

    import

---
+++ src/js/01-gallery.js
@@ -0,0 +1,22 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const markupList = document.querySelector('.gallery');
+
+const markupItems = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+  )
+  .join('');
+
+markupList.insertAdjacentHTML('beforeend', markupItems);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
